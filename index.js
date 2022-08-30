37151795ab03e1548a37e0eb9c84bcf64bf62bc4
Amdesew <96917631+Amdesew@users.noreply.github.com>
2022-08-30 17:43:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,240 @@ function result(){
     let mytext = document.querySelector('#mytext')
     let message = document.querySelector('#message')
 
-    if (mytext.value == 515){
-        message.innerHTML = "ዉጤት: አለሙ ከበደ >>> 82"
+    if (mytext.value == 11828){
+        message.innerHTML = "ዉጤት: አልታሰብ ይስማዉ ሞገስ>>> "
     }
 
-    else if (mytext.value == 615){
-        message.innerHTML = "ዉጤት፡ አበበ ሽመልስ ››› 92 "
+    else if (mytext.value == 94367){
+        message.innerHTML = "ዉጤት፡ እሸቴ አስማማዉ አታለል›››  "
+    }
+
+    else if (mytext.value == 94368){
+        message.innerHTML = "ዉጤት፡ ባለሞል ገብሬ ፍቅር›››  "
+    }
+
+    else if (mytext.value == 94369){
+        message.innerHTML = "ዉጤት፡ ገነቱ ላቀዉ ጥላሁን›››  "
+    }
+
+    else if (mytext.value == 94370){
+        message.innerHTML = "ዉጤት፡ ቢያዝን አዲሱ ጌታሁን›››  "
+    }
+    
+    else if (mytext.value == 94371){
+        message.innerHTML = "ዉጤት፡ ሞትባይኖር አሰፋ ዘለቀ›››  "
+    }
+
+    else if (mytext.value == 94372){
+        message.innerHTML = "ዉጤት፡ አስማማዉ ወርቁ አምሳሉ›››  "
+    }
+
+    else if (mytext.value == 94373){
+        message.innerHTML = "ዉጤት፡ በለጠ አስፋዉ ወርቁ›››  "
+    }
+
+    else if (mytext.value == 94374){
+        message.innerHTML = "ዉጤት፡ ይፍረደዉ መዝገቡ አለሀኝ›››  "
+    }
+
+    else if (mytext.value == 94375){
+        message.innerHTML = "ዉጤት፡ ቁምላቸዉ አሰፋ ፈጠነ›››  "
+    }
+
+    else if (mytext.value == 94376){
+        message.innerHTML = "ዉጤት፡ በሪሁን ዮሁንስ ዘለቀ›››  "
+    }
+
+    else if (mytext.value == 94377){
+        message.innerHTML = "ዉጤት፡ ወርቃለም ነገሰ ገነት›››  "
+    }
+
+    else if (mytext.value == 94378){
+        message.innerHTML = "ዉጤት፡ ሀዮሎም ፈንተንሁ አሊ›››  "
+    }
+
+    else if (mytext.value == 94379){
+        message.innerHTML = "ዉጤት፡ አባይነዉ በልስቲ ጌታሁን›››  "
+    }
+
+    else if (mytext.value == 94390){
+        message.innerHTML = "ዉጤት፡ አታሎ ታዘበዉ ጌጤ››› "
+    }
+
+    else if (mytext.value == 94391){
+        message.innerHTML = "ዉጤት፡ ቴዎድሮስ ሀዋዝ አወቀ›››  "
+    }
+
+    else if (mytext.value == 94392){
+        message.innerHTML = "ዉጤት፡ ተመስገን ምላሹ ታረቀ›››  "
+    }
+
+    else if (mytext.value == 94393){
+        message.innerHTML = "ዉጤት፡ ምናየሁ አለሙ ወርቅነህ›››  "
+    }
+
+    else if (mytext.value == 94394){
+        message.innerHTML = "ዉጤት፡ እምወደዉ ክፍያለዉ አወቀ›››  "
+    }
+
+    else if (mytext.value == 94395){
+        message.innerHTML = "ዉጤት፡ አለሙ ታደለ በላይ›››  "
+    }
+
+    else if (mytext.value == 94396){
+        message.innerHTML = "ዉጤት፡ ተመስገን በላቸዉ ወንዴ›››  "
+    }
+
+    else if (mytext.value == 94397){
+        message.innerHTML = "ዉጤት፡ መንግስቱ ስጦታዉ መኮነን›››  "
+    }
+
+    else if (mytext.value == 94398){
+        message.innerHTML = "ዉጤት፡ አያና እንየዉ ትዛዙ›››  "
+    }
+
+    else if (mytext.value == 94399){
+        message.innerHTML = "ዉጤት፡ ይታየህ አጣናዉ ከበደ›››  "
+    }
+
+    else if (mytext.value == 94400){
+        message.innerHTML = "ዉጤት፡ ቦጋለ ወርቁ አየነዉ›››  "
+    }
+
+    else if (mytext.value == 94401){
+        message.innerHTML = "ዉጤት፡ ቢኒያም ቻሌ ወንዴ›››  "
+    }
+
+    else if (mytext.value == 94402){
+        message.innerHTML = "ዉጤት፡ አዲሱ ታያቸዉ ወንዴ›››  "
+    }
+
+    else if (mytext.value == 94403){
+        message.innerHTML = "ዉጤት፡ ጌታቸዉ ዝናዉ ሹምየ›››  "
+    }
+
+    else if (mytext.value == 94404){
+        message.innerHTML = "ዉጤት፡ ከድር ሞሳ እንድሪስ›››  "
+    }
+
+    else if (mytext.value == 94405){
+        message.innerHTML = "ዉጤት፡ ኑረዲን መሳ ሙሀመድ›››  "
+    }
+
+    else if (mytext.value == 94406){
+        message.innerHTML = "ዉጤት፡ አወቀ ጋሻዉ ቸኮል›››  "
+    }
+
+    else if (mytext.value == 94407){
+        message.innerHTML = "ዉጤት፡ ደመቀ ፍስሀ ወርቁ›››  "
+    }
+
+    else if (mytext.value == 94408){
+        message.innerHTML = "ዉጤት፡ በእዉነቱ አለማየሁ ደበበ›››  "
+    }
+
+    else if (mytext.value == 94409){
+        message.innerHTML = "ዉጤት፡ መልካሙ ጌጡ ታየ›››  "
+    }
+
+    else if (mytext.value == 94410){
+        message.innerHTML = "ዉጤት፡ ተመስገን ቢሻዉ አቤ›››  "
+    }
+
+    else if (mytext.value == 94411){
+        message.innerHTML = "ዉጤት፡ ደረጀ ሙሉጌታ አስፋዉ›››  "
+    }
+
+    else if (mytext.value == 94412){
+        message.innerHTML = "ዉጤት፡ ላቀቸዉ አዉለዉ ጥሩነህ›››  "
+    }
+
+    else if (mytext.value == 94413){
+        message.innerHTML = "ዉጤት፡ ሞላ ተሻሽጎ አግማስ›››  "
+    }
+
+    else if (mytext.value == 94414){
+        message.innerHTML = "ዉጤት፡ እሱያዉቃል ጌትነት አረጋ›››  "
+    }
+
+    else if (mytext.value == 94415){
+        message.innerHTML = "ዉጤት፡ መኩሪያዉ ወርቁ ተመስገን›››  "
+    }
+
+    else if (mytext.value == 94416){
+        message.innerHTML = "ዉጤት፡ አባላዕከማርያም አወቀ ተረፈ›››  "
+    }
+
+    else if (mytext.value == 94417){
+        message.innerHTML = "ዉጤት፡ በላይነህ ባሳዝነዉ አንሙት›››  "
+    }
+
+    else if (mytext.value == 94418){
+        message.innerHTML = "ዉጤት፡ ወርቅነህ ቀሬ ቸኮል›››  "
+    }
+
+    else if (mytext.value == 94419){
+        message.innerHTML = "ዉጤት፡ ጌታነህ ታደለ ይግዛዉ›››  "
+    }
+
+    else if (mytext.value == 94420){
+        message.innerHTML = "ዉጤት፡ ያምራል ጌታነህ ጥሩ›››  "
+    }
+
+    else if (mytext.value == 94421){
+        message.innerHTML = "ዉጤት፡ ጌታቸዉ መልካሙ አበጀ›››  "
+    }
+
+    else if (mytext.value == 94422){
+        message.innerHTML = "ዉጤት፡ ያብባል ካሳ አባተ›››  "
+    }
+
+    else if (mytext.value == 94423){
+        message.innerHTML = "ዉጤት፡ ማሩ ሙሉ ብርሀን›››  "
+    }
+
+    else if (mytext.value == 94424){
+        message.innerHTML = "ዉጤት፡ ታያቸዉ መንጌ በዛብህ›››  "
+    }
+
+    else if (mytext.value == 94425){
+        message.innerHTML = "ዉጤት፡ ዘሪሁን ምናለ ሁነኛዉ›››  "
+    }
+
+    else if (mytext.value == 94426){
+        message.innerHTML = "ዉጤት፡ ደርሶ አለሙ ንጋቴ›››  "
+    }
+
+    else if (mytext.value == 94427){
+        message.innerHTML = "ዉጤት፡ አበበ ሲሳይ እንየዉ›››  "
+    }
+
+    else if (mytext.value == 94428){
+        message.innerHTML = "ዉጤት፡ መልካሙ እንደሻዉ መኮነን›››  "
+    }
+
+    else if (mytext.value == 94429){
+        message.innerHTML = "ዉጤት፡ ቻሌ ዋለልኝ ደርሶ›››  "
+    }
+
+    else if (mytext.value == 94430){
+        message.innerHTML = "ዉጤት፡ አብርሀም ንጉሴ አበበ›››  "
+    }
+
+    else if (mytext.value == 94431){
+        message.innerHTML = "ዉጤት፡ ዋጋ ደሴ አስማረ›››  "
+    }
+
+    else if (mytext.value == 94432){
+        message.innerHTML = "ዉጤት፡ ሙሉጌታ ያረጋል ተፈራ›››  "
+    }
+
+    else if (mytext.value == 94433){
+        message.innerHTML = "ዉጤት፡ አማኑኤል ተስፋ ዳኛዉ›››  "
+    }
+
+    else if (mytext.value == 94434){
+        message.innerHTML = "ዉጤት፡ ተመስገን ተሰራ ገበየ›››  "
     }
 
     else{
